f8b1ccf97a53a368fb564912193b0b26b5f0ba76
enzoze <ordersfromanywhere@gmail.com>
2025-07-14 14:22:37 +0300

    Update core.js

--- core.js
+++ core.js
@@ -1,19 +1,15 @@
 // core.js
 
-// Utility to wait for an element to appear
 function waitForElement(selector, callback, interval = 300, maxAttempts = 50) {
   let attempts = 0;
   const check = () => {
     const el = document.querySelector(selector);
     if (el) return callback(el);
-    if (attempts++ < maxAttempts) {
-      setTimeout(check, interval);
-    }
+    if (attempts++ < maxAttempts) setTimeout(check, interval);
   };
   check();
 }
 
-// Global state management
 window.menuState = {
   megaMenuVisible: false,
   accountMenuVisible: false
@@ -36,35 +32,19 @@ window.loadMenuState = function () {
   }
 };
 
-// Load stored state immediately
 window.loadMenuState();
 
-// Helper: Debounce function
-window.debounce = function (func, wait = 200) {
-  let timeout;
-  return function (...args) {
-    clearTimeout(timeout);
-    timeout = setTimeout(() => func.apply(this, args), wait);
-  };
-};
-
-// MutationObserver for route changes (SPA-like InkSoft behavior)
-window.routeObserver = new MutationObserver(() => {
-  document.dispatchEvent(new Event("route:changed"));
-});
-
-waitForElement("main", (main) => {
-  window.routeObserver.observe(main, { childList: true, subtree: true });
-});
-
-// Polyfill for passive event listeners (performance boost)
-try {
-  const noop = () => {};
-  window.addEventListener("test", noop, { passive: true });
-} catch (err) {
-  EventTarget.prototype.addEventListener = function (type, fn, options) {
-    if (typeof options === "object") options.passive = true;
-    else options = { passive: true };
-    EventTarget.prototype.addEventListener.call(this, type, fn, options);
-  };
+let lastPathname = location.pathname;
+function handleNavigationChange() {
+  if (location.pathname !== lastPathname) {
+    lastPathname = location.pathname;
+    document.dispatchEvent(new Event("route:changed"));
+  }
 }
+function checkForNavigation() {
+  requestAnimationFrame(() => {
+    handleNavigationChange();
+    checkForNavigation();
+  });
+}
+checkForNavigation();
